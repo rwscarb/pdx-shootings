d8fdbb1fd378c3bbaffdfad564165010d60760c2
Ryan Scarbery <i27989@verisk.com>
2022-04-15 08:19:19 -0500

    .

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+echo 'pdx-shootings.com' > CNAME
+
+git init
+git checkout -b master
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:rwscarb/pdx-shootings.git master:gh-pages
+
+cd -
