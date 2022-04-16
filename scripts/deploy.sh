26fa1a16ff297eab6597c74fb9705f3200cbf5cd
Ryan Scarbery <i27989@verisk.com>
2022-04-16 09:52:03 -0700

    keep deploy script for user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,10 @@ cd dist
 
 echo 'map.pdx-shootings.com' > CNAME
 
-git config --global user.email 'email@example.com'
-git config --global user.name 'Automated Deployment'
 git init
 git checkout -b master
 git add -A
+git commit -m 'deploy'
 
 git push -f git@github.com:rwscarb/pdx-shootings.git master:gh-pages
 
