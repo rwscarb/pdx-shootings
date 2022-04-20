3d1d6b327c8d9faf331f558db54fef922f291ba6
Ryan Scarbery <i27989@verisk.com>
2022-04-20 16:07:31 -0700

    return non zero on error

--- scripts/fetch-barrels.py
+++ scripts/fetch-barrels.py
@@ -7,26 +7,35 @@ import requests
 url = ('https://utility.arcgis.com/usrsvcs/servers/1bc5e2ebd3f54f8aac43fe792ae7545b/rest/services/Hosted/'
        'Slow_Streets_Installations/FeatureServer/0/query')
 
-response = requests.get(url, params={
-    'f': 'geojson',
-    'where': "(status = 'ACTIVE') AND (type = 'BARREL')",
-    'returnGeometry': 'true',
-    'spatialRel': 'esriSpatialRelIntersects',
-    'outFields': '*',
-    'orderByFields': 'objectid ASC',
-    'resultOffset': 0,
-    'returnIdsOnly': 'false',
-    'returnCountOnly': 'false',
-    'resultRecordCount': 1000,
-})
 
-if response.ok:
-    features = []
-    data = response.json()
-    for feature in data['features']:
-        feature['properties']['date'] = feature['properties']['created_date']
-        features.append(feature)
-    json.dump({
-        'type': 'FeatureCollection',
-        'features': features,
-    }, open('./public/barrels.geojson', 'w'))
+def main():
+    response = requests.get(url, params={
+        'f': 'geojson',
+        'where': "(status = 'ACTIVE') AND (type = 'BARREL')",
+        'returnGeometry': 'true',
+        'spatialRel': 'esriSpatialRelIntersects',
+        'outFields': '*',
+        'orderByFields': 'objectid ASC',
+        'resultOffset': 0,
+        'returnIdsOnly': 'false',
+        'returnCountOnly': 'false',
+        'resultRecordCount': 1000,
+    })
+
+    if response.ok:
+        features = []
+        data = response.json()
+        for feature in data['features']:
+            feature['properties']['date'] = feature['properties']['created_date']
+            features.append(feature)
+        json.dump({
+            'type': 'FeatureCollection',
+            'features': features,
+        }, open('./public/barrels.geojson', 'w'))
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
--- scripts/fetch-shootings.py
+++ scripts/fetch-shootings.py
@@ -8,34 +8,44 @@ import json
 import requests
 
 url = 'https://public.tableau.com/views/PPBOpenDataDownloads/Shootings.csv?:showVizHome=no'
-response = requests.get(url)
 
-if response.ok:
-    dr = csv.DictReader(response.content.decode('utf-8').splitlines())
-    features = []
-    for row in dr:
-        coordinates = [float(row['Open Data Longitude']), float(row['Open Data Latitude'])]
-        uid = row['Incident Number']
-        incident = {
-            'block_address': row['Block Address'],
-            'id': uid,
-            'date': calendar.timegm(datetime.datetime.strptime(row['Occurence Date'], '%m/%d/%Y').timetuple()) * 1000,
-            'injury': row['Person Injury'].strip().lower() == 'true',
-            'precinct': row['Precinct'],
-            'casings': int(row['CasingsRecovered']),
-            'location': coordinates,
-        }
-        if any(coordinates):
-            features.append({
+
+def main():
+    response = requests.get(url)
+
+    if response.ok:
+        dr = csv.DictReader(response.content.decode('utf-8').splitlines())
+        features = []
+        for row in dr:
+            coordinates = [float(row['Open Data Longitude']), float(row['Open Data Latitude'])]
+            uid = row['Incident Number']
+            incident = {
+                'block_address': row['Block Address'],
                 'id': uid,
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': coordinates
-                },
-                'properties': incident
-            })
-    json.dump({
-        'type': 'FeatureCollection',
-        'features': features,
-    }, open('./public/shootings.geojson', 'w'))
+                'date': calendar.timegm(datetime.datetime.strptime(row['Occurence Date'], '%m/%d/%Y').timetuple()) * 1000,
+                'injury': row['Person Injury'].strip().lower() == 'true',
+                'precinct': row['Precinct'],
+                'casings': int(row['CasingsRecovered']),
+                'location': coordinates,
+            }
+            if any(coordinates):
+                features.append({
+                    'id': uid,
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': coordinates
+                    },
+                    'properties': incident
+                })
+        json.dump({
+            'type': 'FeatureCollection',
+            'features': features,
+        }, open('./public/shootings.geojson', 'w'))
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
