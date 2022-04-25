09880ad4dd2a742d24a5711bd8dcf28537a1ded5
Ryan Scarbery <i27989@verisk.com>
2022-04-25 16:40:20 -0700

    remove comment

--- src/constants.js
+++ src/constants.js
@@ -68,11 +68,6 @@ export const CLUSTER_LAYER = {
         visibility: 'visible'
     },
     paint: {
-        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-        // with three steps to implement three types of circles:
-        //   * Blue, 20px circles when point count is less than 100
-        //   * Yellow, 30px circles when point count is between 100 and 750
-        //   * Pink, 40px circles when point count is greater than or equal to 750
         'circle-color': [
             'step',
             ['get', 'point_count'],
