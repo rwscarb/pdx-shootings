5379f79e1cb9b3375c6f3178e2c9bef3a1d57f23
Ryan Scarbery <i27989@verisk.com>
2022-05-02 09:15:15 -0700

    add sentry (#5)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
 import { Vue3Mq } from 'vue3-mq'
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
 
 import App from './App.vue'
 
+
 const app = createApp(App);
 app.use(Vue3Mq);
+
+if (import.meta.env.PROD) {
+  Sentry.init({
+    app,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      new BrowserTracing,
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,31 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteSentry from 'vite-plugin-sentry'
+
+const plugins = [vue()];
+
+if (process.env.VITE_SENTRY_DSN) {
+  const sentryConfig  = {
+    authToken: process.env.VITE_SENTRY_DSN,
+    org: 'ryan-scarbery',
+    project: 'pdx-shootings',
+    deploy: {
+      env: 'production'
+    },
+    setCommits: {
+      auto: true
+    },
+    sourceMaps: {
+      include: ['./dist/assets'],
+    }
+  }
+  plugins.push(viteSentry(sentryConfig))
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
