fb8eb69f1ab44462f12bca968a8dbaf1cdb3ac29
Ryan Scarbery <ryan.scarbery@gmail.com>
2022-05-03 22:08:29 -0700

    query params

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import { Vue3Mq } from 'vue3-mq'
 import * as Sentry from '@sentry/vue'
 import { BrowserTracing } from '@sentry/tracing'
 
 import App from './App.vue'
+import Map from './Map.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{
+    path: '/:catchAll(.*)',
+    component: Map
+  }],
+});
 
 
 const app = createApp(App);
 app.use(Vue3Mq);
+app.use(router);
 
 if (import.meta.env.PROD) {
   Sentry.init({
